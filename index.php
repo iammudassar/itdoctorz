8ea0205e1fb542e5cc5dcbee9e5f240375dbe925
iammudassar <57129405+iammudassar@users.noreply.github.com>
2021-08-20 23:16:30 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *  Main template file 
+ * 
+ * @package itdoctorz
+ */
+?>
+
+<html>
+    <head>
+        <meta charset="UTF-8" >
+    
+        <title>IT Doctorz</title>
+    </head>
+
+    <body>
+        <H1> WELCOME TO IT DOCTORZ </H1>
+    </body>
+</html>
